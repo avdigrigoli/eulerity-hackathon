71b3fdbc1b28050ead30cd77373a1f471505563f
avdigrigoli <adigrigs@gmail.com>
2026-05-28 14:10:19 -0400

    Added smooth scroll and changed global colors etc.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import "./styles/globals.css";
 
 import { SelectionProvider } from "./context/SelectionContext";
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
+import { lenis } from "./utils/smoothScroll.ts";
 
 export default function App() {
-  return (
-      <SelectionProvider>
-        <AppRoutes />
-      </SelectionProvider>
-  );
+    useEffect(() => {
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+    }, []);
+
+    return (
+        <SelectionProvider>
+            <AppRoutes />
+        </SelectionProvider>
+    );
 }
